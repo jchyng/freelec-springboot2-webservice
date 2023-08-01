a59a0389488e1043b2194e2a3d4cc5f2d5f558e0
정찬영 <jcy0342@gmail.com>
2023-08-01 16:10:37 +0900

    encoding 수정

--- deploy.sh
+++ deploy.sh
@@ -16,19 +16,18 @@ echo "> step1 디렉토리로 이동"
 cd $REPOSITORY
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/$PROJECT_NAME/build/libs/$PROJECT_FILENAME $REPOSITORY/
 
+# 프로세스 이름으로 PID 검색
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f "$PROJECT_FILENAME")
-
-echo "> 현재 구동 중인 애플리케이션 PID: $CURRENT_PID"
+CURRENT_PID=$(pgrep -f "${PROJECT_FILENAME}")
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "현재 실행 중인 $PROJECT_FILENAME 프로세스가 없습니다."
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+    echo "현재 실행 중인 $PROJECT_FILENAME 프로세스 PID: $CURRENT_PID"
+    echo "프로세스를 종료합니다."
+    kill -15 $CURRENT_PID
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -36,4 +35,3 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $PROJECT_FILENAME > $REPOSITORY/nohup.out 2>&1 &
-
